fca521db23c9330e1b3f7c1ea1dd3104188f5b71
rojalingga <rojalingga2006@gmail.com>
2024-10-30 12:14:47 +0700

    Percobaan 4 Rata Rata Nilai

---
+++ RataNilai_20.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RataNilai_20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i,j;
+        float totalNilai, nilaiMhs, rataNilai;
+        i = 1;
+        while (i <= 5) {
+            System.out.println("Input Nilai Mahasiswa ke " +i);
+            totalNilai = 0;
+            for (j = 1; j <= 5; j++) {
+                System.out.print("Nilai ke-" + j + " = ");
+                nilaiMhs = sc.nextInt();
+                totalNilai += nilaiMhs;
+            }
+            rataNilai = totalNilai / 5;
+            System.out.println("Rata - rata Nilai Mahasiswa ke " +i+ " adalah " +rataNilai);
+            i++;
+        }
+
+    }
+}
